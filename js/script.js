93eca074614ab59689f41895dc74d7a03d710e08
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-23 17:20:29 +0200

    function for moves
    created a function for the moves, works for Pokémon with lots of moves, not for Ditto or Smeargle who don't have a lot of moves

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const apiPokemon = "https://pokeapi.co/api/v2/pokemon/";
 const searchButton = document.getElementById('search');
 
-
 async function getPokemon () {
     const pokemonValue = document.getElementById('pokemon').value;
     let input = pokemonValue;
@@ -11,8 +10,9 @@ async function getPokemon () {
     if (!response.ok) {
         throw 'Something went wrong!';
     }
+
     const data = await response.json();
-    console.log(data, data.id, data.name, data.moves[0], data.moves[1], data.moves[2], data.moves[3], data.sprites.front_default);
+    //console.log(data, data.id, data.name, data.moves[0], data.moves[1], data.moves[2], data.moves[3], data.sprites.front_default);
     //display the ID
     let pokemonID = document.getElementById('pokemonID');
     pokemonID.innerText = data.id;
@@ -21,18 +21,24 @@ async function getPokemon () {
     pokemonName.innerText = data.name;
     //display the moves
     let pokemonMoves = document.getElementById('pokemonMoves');
-    pokemonMoves.innerText = `${data.moves[0].move.name}, ${data.moves[1].move.name}, ${data.moves[2].move.name}, ${data.moves[3].move.name}`;
+    function moves () {
+        const moves = [];
+        for (let i = 0; i < 4; i++) {
+            moves.push(` ${data.moves[i].move.name}`);
+        }
+        return moves;
+    }
+    pokemonMoves.innerText = moves();
     //display the image
     function addImage (){
-        const pokemonImage = document.getElementById('pokemonImage');
+        const pokemonImageContainer = document.getElementById('pokemonImageContainer');
         let newPokemonImage = new Image;
         newPokemonImage.src = data.sprites.front_default;
-        pokemonImage.innerHTML = '';
-        pokemonImage.appendChild(newPokemonImage);
+        pokemonImageContainer.innerHTML = '';
+        pokemonImageContainer.appendChild(newPokemonImage);
     }
 
     addImage();
-
 }
 
 searchButton.addEventListener('click', getPokemon);
