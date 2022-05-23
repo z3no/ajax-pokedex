df748d3efdf76fac1b4491b2ac4dfb0ed9f07f31
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-23 15:12:31 +0200

    displaying input values for name and moves, next up image

--- js/script.js
+++ js/script.js
@@ -12,10 +12,17 @@ async function getPokemon () {
         throw 'Something went wrong!';
     }
     const data = await response.json();
-    console.log(data.id, data.name, data.moves[0], data.moves[1], data.moves[2], data.moves[3], data.sprites.front_default);
+    console.log(data, data.id, data.name, data.moves[0], data.moves[1], data.moves[2], data.moves[3], data.sprites.front_default);
     //display the ID
     let pokemonID = document.getElementById('pokemonID');
     pokemonID.innerText = data.id;
+    //display the Pokémon name
+    let pokemonName = document.getElementById('pokemonName');
+    pokemonName.innerText = data.name;
+    //display the moves
+    let pokemonMoves = document.getElementById('pokemonMoves');
+    pokemonMoves.innerText = `${data.moves[0].move.name}, ${data.moves[1].move.name}, ${data.moves[2].move.name}, ${data.moves[3].move.name}`;
+    //display the image
 }
 
 searchButton.addEventListener('click', getPokemon);
