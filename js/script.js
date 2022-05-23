6841fa822835b7baff95db1071bbd51ce8b76977
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-23 11:38:35 +0200

    getting the data from api

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-(() => {
-    const searchButton = document.getElementById('search');
+
+    const apiPokemon = "https://pokeapi.co/api/v2/pokemon/";
+    //const searchButton = document.getElementById('search');
+    //searchButton.addEventListener('click', getPokemonById);
 
     async function getPokemonById () {
-        const pokemonId = document.getElementById('pokemon-id');
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+        //let pokemonId = document.getElementById('pokemon-id').value;
+        //let numberId = Number(pokemonId);
+
+        const response = await fetch(apiPokemon);
         // If call failed, giva an error
         if (!response.ok) {
             throw 'Something went wrong!';
@@ -11,4 +15,5 @@
         const data = await response.json();
         console.log(data);
     }
-})
+
+    getPokemonById();
