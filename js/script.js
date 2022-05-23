3b1f3c064efa3d4826ec4c0c3a94e2f94537da3e
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-23 12:26:45 +0200

    console logged name of id you put in

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
+const apiPokemon = "https://pokeapi.co/api/v2/pokemon/";
+const searchButton = document.getElementById('search');
 
-    const apiPokemon = "https://pokeapi.co/api/v2/pokemon/";
-    //const searchButton = document.getElementById('search');
-    //searchButton.addEventListener('click', getPokemonById);
+async function getPokemonById () {
+    let pokemonId = document.getElementById('pokemon-id').value;
+    let numberId = Number(pokemonId);
 
-    async function getPokemonById () {
-        //let pokemonId = document.getElementById('pokemon-id').value;
-        //let numberId = Number(pokemonId);
-
-        const response = await fetch(apiPokemon);
-        // If call failed, giva an error
-        if (!response.ok) {
-            throw 'Something went wrong!';
-        }
-        const data = await response.json();
-        console.log(data);
+    const response = await fetch(`${apiPokemon}${numberId}`);
+    // If call failed, giva an error
+    if (!response.ok) {
+        throw 'Something went wrong!';
     }
+    const data = await response.json();
+    console.log(data.name);
+}
 
-    getPokemonById();
+searchButton.addEventListener('click', getPokemonById);
