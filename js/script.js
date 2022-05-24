087b3368f9b8278a619ae4ad7f32c181867ed7cd
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-24 17:02:13 +0200

    if there is no evolution
    created an if else, for if there is no previous evolution it console logs a message

--- js/script.js
+++ js/script.js
@@ -15,18 +15,6 @@ async function getPokemon () {
         const data = await response.json();
         //console.log(data, data.id, data.name, data.moves[0], data.moves[1], data.moves[2], data.moves[3], data.sprites.front_default);
 
-    /*
-    //get the evolution line
-        const responseEvolutionLine = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${data.name}`);
-        if(!responseEvolutionLine.ok) {
-            throw 'Something went wrong!';
-        }
-        const dataEvolutionLine = await responseEvolutionLine.json();
-        console.log(dataEvolutionLine);
-
-        const previousEvolution = dataEvolutionLine.evolves_from_species.name;
-        console.log(previousEvolution);
-    */
 
     //display the ID
         let pokemonID = document.getElementById('pokemonID');
@@ -68,3 +56,34 @@ async function getPokemon () {
 }
 
 searchButton.addEventListener('click', getPokemon);
+searchButton.addEventListener('click', getEvolutions);
+
+    //get the evolution line
+    async function getEvolutions() {
+        const pokemonValue = document.getElementById('pokemon').value;
+        let input = pokemonValue;
+
+        //get pokemon by input
+        const response = await fetch(`${apiPokemon}${input}`);
+        // If call failed, give an error
+        if (!response.ok) {
+            throw 'Something went wrong!';
+        }
+        const data = await response.json();
+
+        const responseEvolutionLine = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${data.name}`);
+        if(!responseEvolutionLine.ok) {
+            throw 'Something went wrong!';
+        }
+        const dataEvolutionLine = await responseEvolutionLine.json();
+        console.log(dataEvolutionLine);
+
+        const previousEvolution = dataEvolutionLine.evolves_from_species;
+        console.log(previousEvolution);
+
+        if (previousEvolution === null) {
+            console.log('No previous evolution');
+        } else {
+            console.log(previousEvolution);
+        }
+    }
