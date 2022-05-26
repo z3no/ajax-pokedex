6d1afbcdd5e3a10b49b2ad5090cf1b282d15a86d
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-26 17:47:12 +0200

    show images of evolutions
    shows the evolution for pokémon who have multiple evolutions, ditto etc still bugs

--- js/test.js
+++ js/test.js
@@ -13,34 +13,40 @@ async function getPokemon () {
         throw 'Something went wrong!';
     }
     const data = await response.json();
-    console.log(data);
+    //console.log(data);
     //console.log(data, data.id, data.name, data.moves[0], data.moves[1], data.moves[2], data.moves[3], data.sprites.front_default);
 
     //get pokemon species url
     const responseSpecies = await fetch (data.species.url);
     const dataSpecies = await responseSpecies.json();
-    console.log(dataSpecies);
+    //console.log(dataSpecies);
 
     //get pokemon evolution chain
     const responseEvolutionChain = await fetch (dataSpecies.evolution_chain.url);
     const dataEvolutionChain = await responseEvolutionChain.json();
-    console.log(dataEvolutionChain);
+    //console.log(dataEvolutionChain);
 
     //evolutions
-    const firstPokemonForm = dataEvolutionChain.chain.species.name;
-    console.log(firstPokemonForm);
+    const firstForm = dataEvolutionChain.chain.species.name;
+    const firstFormDataFetch = await fetch(`${apiPokemon}${firstForm}`);
+    const firstFormData = await firstFormDataFetch.json();
+    console.log(firstFormData);
 
-    const firstEvolutionName = dataEvolutionChain.chain.evolves_to[0].species.name;
-    console.log(firstEvolutionName);
+    const firstEvolution = dataEvolutionChain.chain.evolves_to[0].species.name;
+    const firstEvolutionDataFetch = await fetch(`${apiPokemon}${firstEvolution}`);
+    const firstEvolutionData = await firstEvolutionDataFetch.json();
+    console.log(firstEvolutionData);
 
-    const secondEvolutionName = dataEvolutionChain.chain.evolves_to[0].evolves_to[0].species.name;
-    console.log(secondEvolutionName);
+    const secondEvolution = dataEvolutionChain.chain.evolves_to[0].evolves_to[0].species.name;
+    const secondEvolutionDataFetch = await fetch(`${apiPokemon}${secondEvolution}`);
+    const secondEvolutionData = await secondEvolutionDataFetch.json();
+    console.log(secondEvolutionData);
 
     //Pokemon with evolutions in array
     let pokemonArray = [];
-    pokemonArray.push(firstPokemonForm);
-    pokemonArray.push(firstEvolutionName);
-    pokemonArray.push(secondEvolutionName);
+    pokemonArray.push(firstForm);
+    pokemonArray.push(firstEvolution);
+    pokemonArray.push(secondEvolution);
 
     console.log(pokemonArray);
 
@@ -48,11 +54,8 @@ async function getPokemon () {
     function displayEvolutions (){
         const evolutionDisplay = document.getElementById('evolutionContainer');
         evolutionDisplay.innerText = pokemonArray.join(' ');
-        pokemonArray.src = data.sprites.front_default;
         console.log(pokemonArray);
     }
-
-    displayEvolutions();
 
     //display the ID
     let pokemonID = document.getElementById('pokemonID');
@@ -73,16 +76,44 @@ async function getPokemon () {
         pokemonMoves.innerText = data.moves.map(moves => moves.move.name).splice(0, 4).join(', ');
     }
 
-    //display the image
+    //display the images
     function addImage (){
         const pokemonImageContainer = document.getElementById('pokemonImageContainer');
         let newPokemonImage = new Image;
         newPokemonImage.src = data.sprites.other.home.front_default;
         pokemonImageContainer.innerHTML = '';
         pokemonImageContainer.appendChild(newPokemonImage);
+
+    }
+    addImage();
+
+    function showFirstForm () {
+        const container = document.getElementById('firstForm');
+        let firstFormImage = new Image(100,100);
+        firstFormImage.src = firstFormData.sprites.other.home.front_default;
+        container.innerHTML = '';
+        container.appendChild(firstFormImage);
     }
 
-    addImage();
+    function showFirstEvolution () {
+        const container = document.getElementById('firstEvolution');
+        let firstEvolutionImage = new Image(100,100);
+        firstEvolutionImage.src = firstEvolutionData.sprites.other.home.front_default;
+        container.innerHTML = '';
+        container.appendChild(firstEvolutionImage);
+    }
+
+    function showSecondEvolution () {
+        const container = document.getElementById('secondEvolution');
+        let secondEvolutionImage = new Image(100,100);
+        secondEvolutionImage.src = secondEvolutionData.sprites.other.home.front_default;
+        container.innerHTML = '';
+        container.appendChild(secondEvolutionImage);
+    }
+
+    showFirstForm();
+    showFirstEvolution();
+    showSecondEvolution();
 
     //make the card visible in HTML
     function displayCard () {
